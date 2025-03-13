d2f6a71f5357e295203a02b23d7bf2909fc57276
MonaEis <mona.eismann@gmx.de>
2025-03-13 19:23:43 +0100

    Tag 11 Bonus Übung Ergänzung

--- src/11_bom_ts_bonus_3_2/bom.ts
+++ src/11_bom_ts_bonus_3_2/bom.ts
@@ -1,11 +1,42 @@
 const timeOutput = document.getElementById("time");
-const timeInputUser = document.getElementById("minutes");
+const timeInputUser = document.getElementById("minutes") as HTMLInputElement;
 
 const startBtn = document.getElementById("start-button");
 const pauseBtn = document.getElementById("pause-button");
 const restartBtn = document.getElementById("restart-button");
 
-if (timeOutput) {
-    let counter = 0;
-    timeOutput.innerHTML = counter.toString();
+let counter = 0;
+let intervallCountdown: number;
+
+startBtn?.addEventListener("click", () => startCountDown());
+
+function formatTime(seconds: number): string {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    const formattedMinutes = String(minutes).padStart(2, "0");
+    const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+    return `${formattedMinutes}:${formattedSeconds}`
+}
+
+
+function startCountDown() {
+    if (timeInputUser) {
+        counter = Number(timeInputUser.value) * 60;
+    }
+
+    if (timeOutput) {
+        timeOutput.innerHTML = formatTime(counter);
+    }
+
+    intervallCountdown = setInterval(() => {
+        counter--;
+        if(timeOutput) timeOutput.innerText = formatTime(counter);
+        
+        if(counter <= 0) {
+            clearInterval(intervallCountdown);
+            alert("Countdown beendet!")
+        }
+    }, 1000)
 }
